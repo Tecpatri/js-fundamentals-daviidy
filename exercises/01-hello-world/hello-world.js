fc06b1bbad0bbafe990858ddd9870d1295ca0105
David <yaodavidarmel@gmail.com>
2025-11-28 00:31:16 +0000

    did exercise 1

--- exercises/01-hello-world/hello-world.js
+++ exercises/01-hello-world/hello-world.js
@@ -2,14 +2,17 @@
 
 export function hello () {
   // Return the string "Hello, world!"
+  return 'Hello, world!';
 }
 
 export function greet (name) {
   // Return a greeting message for the given name
   // Example: greet("Alice") should return "Hello, Alice!"
+  return `Hello, ${name}`;
 }
 
 export function showMessage (message) {
   // Return the message as-is
   // This simulates displaying a message to the user
+  return message;
 }
